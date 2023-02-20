7c71131b33f3935b3246076a51effc429b119e73
Bruno Gomes <brunosoges@gmail.com>
2023-02-20 00:08:33 -0300

    add: Defining a class and accessing its attributes.

---
+++ poo/classes.groovy
@@ -0,0 +1,23 @@
+class Pessoa implements Serializable {
+    String nome
+    String sobreNome
+    boolean ativa
+
+    String getNome() {
+        "O nome é $nome"
+    }
+}
+
+// O métodos getters e setters são criados automaticamente pelo Groovy
+Pessoa pessoa = new Pessoa()
+pessoa.setNome("Bruno")
+
+// OBS > é possivel acessar os atributos da classe tanto pelo getter como pelo próprio atributo.
+println pessoa.getNome()
+println pessoa.nome
+
+// Construtores dinamicos
+Pessoa pessoa2 = new Pessoa(nome: "Pedro", sobreNome: "da Silva", ativa: true)
+println "$pessoa2.nome $pessoa2.sobreNome esta com situação $pessoa2.ativa"
+
+println pessoa2.@nome // Com o '@' acessa diretamente o atributo presente na classe.
